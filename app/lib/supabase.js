6ff373e9a35363843498b769bd80937ce3f406ec
AM-creates <antonisminas23@gmail.com>
2026-06-21 13:19:29 +0300

    fix anon key

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://heysxxdhwzegvizzfxfu.supabase.co'
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_TRzpC12Owty3TU6DTMebAA_7tzeYOUV'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhleXN4eGRod3plZ3ZpenpmeGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE3NzMyOTUsImV4cCI6MjA5NzM0OTI5NX0.WoUMG3sEIN-Z8jzK_XfN38C49Qnm5SUAleNTPan7mL8'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
