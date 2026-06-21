1bbebb042c91c54148f9a38d584e2998c6580787
AM-creates <antonisminas23@gmail.com>
2026-06-21 13:12:29 +0300

    fix supabase config

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://heysxxdhwzegvizzfxfu.supabase.co'
-const supabaseKey = 'sb_publishable_TRzpC12Owty3TU6DTMebAA_7tzeYOUV'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://heysxxdhwzegvizzfxfu.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || 'sb_publishable_TRzpC12Owty3TU6DTMebAA_7tzeYOUV'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
